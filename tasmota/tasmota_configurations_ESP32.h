4379356facdff30da0fb1bfdc7254d8a9be0ed9d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-06-15 16:55:31 +0200

    more shrink

--- tasmota/tasmota_configurations_ESP32.h
+++ tasmota/tasmota_configurations_ESP32.h
@@ -239,15 +239,22 @@
 #undef USE_SHELLY_DIMMER                        // Disable support for Shelly Dimmer (+3k code)
 
 #define USE_LIGHT_PALETTE                        // Add support for color palette (+0k9 code)
+#undef USE_EMULATION_WEMO
+//#undef USE_EMULATION_HUE
 
 #define USE_DS18x20                              // Add support for DS18x20 sensors with id sort, single scan and read retry (+1k3 code)
 
+#undef USE_ENERGY_SENSOR
+
+
 #define USE_I2C                                  // I2C using library wire (+10k code, 0k2 mem, 124 iram)
+#undef USE_MLX90614
+
 #define USE_SHT                                // [I2cDriver8] Enable SHT1X sensor (+1k4 code)
 #define USE_HTU                                // [I2cDriver9] Enable HTU21/SI7013/SI7020/SI7021 sensor (I2C address 0x40) (+1k5 code)
 #define USE_BMP                                // [I2cDriver10] Enable BMP085/BMP180/BMP280/BME280 sensors (I2C addresses 0x76 and 0x77) (+4k4 code)
 //  #define USE_BME680                           // Enable support for BME680 sensor using Bosch BME680 library (+4k code)
-#define USE_BH1750                             // [I2cDriver11] Enable BH1750 sensor (I2C address 0x23 or 0x5C) (+0k5 code)
+//#define USE_BH1750                             // [I2cDriver11] Enable BH1750 sensor (I2C address 0x23 or 0x5C) (+0k5 code)
 //#define USE_VEML6070                           // [I2cDriver12] Enable VEML6070 sensor (I2C addresses 0x38 and 0x39) (+1k5 code)
 //#define USE_VEML6075                           // [I2cDriver49] Enable VEML6075 UVA/UVB/UVINDEX Sensor (I2C address 0x10) (+2k1 code)
 //#define USE_VEML7700                           // [I2cDriver50] Enable VEML7700 Ambient Light sensor (I2C addresses 0x10) (+4k5 code)
