864b8cff3ef80a0b785ca9dc9f9f27bb04e5bc98
Jason2866 <obermeier.johann@googlemail.com>
2022-01-22 11:05:31 +0000

    no shutter

--- tasmota/tasmota_configurations_ESP32.h
+++ tasmota/tasmota_configurations_ESP32.h
@@ -56,6 +56,7 @@
 #undef USE_DS18x20
 #undef USE_WS2812
 #undef USE_ENERGY_SENSOR
+#undef USE_SHUTTER
 #undef USE_BERRY                                 // Disable Berry scripting language
 #undef USE_MI_ESP32                             // (ESP32 only) Disable support for ESP32 as a BLE-bridge (+9k2 mem, +292k flash)
 #undef USE_BLE_ESP32
