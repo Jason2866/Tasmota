4b00872cb706fb99d525f1730e92fe41d0b5cecb
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-06-01 15:20:44 +0200

    patch pioino for folder use

--- pio-tools/add_src_folder.py
+++ pio-tools/add_src_folder.py
@@ -1,8 +1,27 @@
-import os
+import site
+from os.path import join, isfile
 
 Import("env")
+env = DefaultEnvironment()
 
-env.BuildSources(
-    os.path.join("$BUILD_DIR", "external", "build"),
-    os.path.join("$PROJECT_DIR", "external", "sources")
-)
+project_dir = join(env.subst("$PROJECT_DIR"))
+sitepackages_dir = "".join(site.getsitepackages())
+patchflag_path = join(sitepackages_dir, "platformio", "builder", "tools", ".patching-done")
+original_file = join(sitepackages_dir, "platformio", "builder", "tools", "pioino.py")
+patched_file = join(project_dir, "pio-tools", "patches", "1-platformio-ino-folders.patch")
+print("original_file", original_file)
+print("patched_file", patched_file)
+
+
+# patch file only if we didn't do it before
+if not isfile(patchflag_path):
+    isfile(original_file) and isfile(patched_file)
+    env.Execute("patch %s %s" % (original_file, patched_file))
+    # env.Execute("touch " + patchflag_path)
+
+
+    def _touch(path):
+        with open(path, "w") as fp:
+            fp.write("")
+
+    env.Execute(lambda *args, **kwargs: _touch(patchflag_path))
