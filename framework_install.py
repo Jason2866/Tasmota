c17499c46f5f3780a260aab17b8321a435ead5ef
Christian Baars <Baars@gmx.de>
2022-10-25 21:42:40 +0200

    Update framework_install.py

--- framework_install.py
+++ framework_install.py
@@ -1,5 +1,6 @@
 import requests
 import subprocess
+import json
 
 
 tasmota32_ini_url = "https://raw.githubusercontent.com/arendst/Tasmota/development/platformio_tasmota32.ini"
@@ -11,9 +12,18 @@ for line in r.iter_lines():
     if "platform" == items[0].strip():
         framework = items[1].strip()
         print (framework)
-cmd = ("pio","pkg","install", framework)
+cmd = ("pio","platform","install", framework)
 returncode = subprocess.call(cmd, shell=False)
 if returncode == 0:
     print("Framework installed ...")
 else:
     print("Could not install Framework!!")
+    
+with open(".platformio/platforms/espressif32/platform.json") as json_file:
+        data = json.load(json_file)
+        for package in data['packages']:
+            print(package)
+    
+
+
+
