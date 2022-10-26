7ca51f9658c75adb3ff4f45ad49b5f5be4381de5
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-10-26 12:05:53 +0200

    copy only one time

--- .github/scripts/framework_install.py
+++ .github/scripts/framework_install.py
@@ -28,16 +28,16 @@ with open("/home/runner/.platformio/platforms/espressif32/platform.json") as jso
 
 safeboot_dir = "./firmware/firmware"
 variants_dir = "/home/runner/.platformio/packages/framework-arduinoespressif32/variants/tasmota"
-if os.path.exists(variants_dir):
-    shutil.rmtree(variants_dir)
-shutil.copytree(safeboot_dir, variants_dir)
+if not os.path.exists(variants_dir):
+    #shutil.rmtree(variants_dir)
+    shutil.copytree(safeboot_dir, variants_dir)
 
 variants_dir = "/home/runner/.platformio/packages/framework-arduino-ITEAD/variants/tasmota"
-if os.path.exists(variants_dir):
-    shutil.rmtree(variants_dir)
-shutil.copytree(safeboot_dir, variants_dir)
+if not os.path.exists(variants_dir):
+    #shutil.rmtree(variants_dir)
+    shutil.copytree(safeboot_dir, variants_dir)
 
 variants_dir = "/home/runner/.platformio/packages/framework-arduino-solo1/variants/tasmota"
-if os.path.exists(variants_dir):
-    shutil.rmtree(variants_dir)
-shutil.copytree(safeboot_dir, variants_dir)
+if not os.path.exists(variants_dir):
+    #shutil.rmtree(variants_dir)
+    shutil.copytree(safeboot_dir, variants_dir)
