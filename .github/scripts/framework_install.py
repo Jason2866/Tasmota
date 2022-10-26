2cc4fc7b6bafc43f6f7be10d3e75cae85e5f9243
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-10-26 11:26:57 +0200

    no UTF decode

--- .github/scripts/framework_install.py
+++ .github/scripts/framework_install.py
@@ -1,21 +1,16 @@
 import os
-import requests
 import subprocess
 import json
 import shutil
 
-#tasmota32_ini_url = "https://raw.githubusercontent.com/arendst/Tasmota/development/platformio_tasmota32.ini"
-#print("Download ",tasmota32_ini_url)
-#r = requests.get(tasmota32_ini_url, stream=True)
 r = open("/home/runner/work/Tasmota/Tasmota/platformio_tasmota32.ini","r+")
 print ("Pio file: ", r.name)
 framework = ""
 for line in r:
-    items = line.decode('utf-8').split("=")
+    items = line.split("=")
     if "platform" == items[0].strip():
         framework = items[1].strip()
         print (framework)
-# cmd = ("pio","platform","install", framework)
 cmd = ("pio","pkg","install","-p", framework)
 returncode = subprocess.call(cmd, shell=False)
 if returncode == 0:
@@ -27,7 +22,6 @@ with open("/home/runner/.platformio/platforms/espressif32/platform.json") as jso
         data = json.load(json_file)
         for package in data['packages']:
             if "framework-arduino" in package:
-                # cmd = ("pio","platform","install","--with-package",data['packages'][package]['version'])
                 cmd = ("pio","pkg","install","-p",data['packages'][package]['version'])
                 print(cmd)
                 returncode = subprocess.call(cmd, shell=False)
