f3e87e7b6a502028d1eefbb3ffdd7e0ebf8e1a11
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-17 20:49:38 +0200

    Update post_esp32.py

--- pio-tools/post_esp32.py
+++ pio-tools/post_esp32.py
@@ -142,15 +142,14 @@ def esp32_copy_new_safeboot_bin(tasmota_platform,new_local_safeboot_fw):
     if os.path.exists(variants_dir):
         shutil.copy(new_local_safeboot_fw, safeboot_fw_name)
 
-def esp32_create_combined_bin(source, target, env):
-    #print("Generating combined binary for serial flashing")
-    new_file_name = env.subst("$BUILD_DIR/${PROGNAME}.factory.bin")
-    sections = env.subst(env.get("FLASH_EXTRA_IMAGES"))
-    firmware_name = env.subst("$BUILD_DIR/${PROGNAME}.bin")
-    chip = env.get("BOARD_MCU")
-    tasmota_platform = esp32_create_chip_string(chip)
-    
-    if "safeboot" not in tasmota_platform:
+if "safeboot" not in tasmota_platform:
+    def esp32_create_combined_bin(source, target, env):
+        #print("Generating combined binary for serial flashing")
+        new_file_name = env.subst("$BUILD_DIR/${PROGNAME}.factory.bin")
+        sections = env.subst(env.get("FLASH_EXTRA_IMAGES"))
+        firmware_name = env.subst("$BUILD_DIR/${PROGNAME}.bin")
+        chip = env.get("BOARD_MCU")
+        tasmota_platform = esp32_create_chip_string(chip)
         print("Generating combined binary for serial flashing")
         # The offset from begin of the file where the app0 partition starts
         # This is defined in the partition .csv file
