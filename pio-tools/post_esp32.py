3d04216fc4a21dbf520cc50ff2852fbcc5b5bb16
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-19 14:01:04 +0200

    Update post_esp32.py

--- pio-tools/post_esp32.py
+++ pio-tools/post_esp32.py
@@ -186,11 +186,12 @@ def esp32_create_combined_bin(source, target, env):
                 #     factory_offset = int(row[3],base=16)
                 elif(row[0] == 'spiffs'):
                     partition_size = row[4]
-                    upload_maximum_mb = env.BoardConfig().get("upload.flash_size","4MB")
+                    upload_maximum_mb = env.BoardConfig().get("upload.flash_size","")
                     print("Board get max upload mb: ", upload_maximum_mb)
                     upload_maximum_size = int(upload_maximum_mb.split("MB")[0]) * 0x100000
                     print("upload_maximum_size: ", upload_maximum_size)
                     partition_size =  hex(upload_maximum_size - int(row[3],base=16))
+                    print("partition_size: ", partition_size)
                     patch_partitions_bin(partition_size)
 
     new_file_name = env.subst("$BUILD_DIR/${PROGNAME}.factory.bin")
