91aaf16fa80403de495608ed105bb8efb8193d26
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-19 13:37:48 +0200

    Update post_esp32.py

--- pio-tools/post_esp32.py
+++ pio-tools/post_esp32.py
@@ -186,7 +186,7 @@ def esp32_create_combined_bin(source, target, env):
                 elif(row[0] == 'spiffs'):
                     partition_size = row[4]
                     upload_maximum_mb = env.BoardConfig().get("upload.maximum_size","4MB")
-                    print("Board get max upload: " + upload_maximum_mb)
+                    print("Board get max upload: ", upload_maximum_mb)
                     upload_maximum_size = int(upload_maximum_mb.split("MB")[0]) * 0x100000
                     partition_size =  hex(upload_maximum_size - int(row[3],base=16))
                     patch_partitions_bin(partition_size)
