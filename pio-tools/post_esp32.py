1e5ce11852a813a0ee1c153fb8086f12b562076b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-10-26 23:46:11 +0200

    rm debug print

--- pio-tools/post_esp32.py
+++ pio-tools/post_esp32.py
@@ -276,7 +276,7 @@ def esp32_create_combined_bin(source, target, env):
             print("Will use custom upload command for flashing operation to add file system defined for this build target.")
 
     if("safeboot" not in firmware_name):
-        print('Using esptool.py arguments: %s' % ' '.join(cmd))
+        #print('Using esptool.py arguments: %s' % ' '.join(cmd))
         esptool.main(cmd)
 
 
