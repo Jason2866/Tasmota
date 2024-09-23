266985bef865ebd43f8ff253e2b496a51ccd7f7b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-09-23 11:42:04 +0200

    idf pio script

--- pio-tools/pre_source_dir.py
+++ pio-tools/pre_source_dir.py
@@ -2,6 +2,8 @@ Import("env")
 
 import glob
 import os
+from os.path import join
+import shutil
 
 def FindInoNodes(env):
     src_dir = glob.escape(env.subst("$PROJECT_SRC_DIR"))
@@ -10,6 +12,56 @@ def FindInoNodes(env):
     )
 
 env.AddMethod(FindInoNodes)
+
+def HandleArduinoIDFbuild(env):
+    print("IDF build!!!, removing LTO")
+    new_build_flags = [f for f in env["BUILD_FLAGS"] if "-flto=auto" not in f]
+    new_build_flags.append("-mtext-section-literals") # TODO
+    env["BUILD_FLAGS"] = new_build_flags
+    print(new_build_flags)
+
+    platform = env.PioPlatform()
+    board = env.BoardConfig()
+    mcu = board.get("build.mcu", "esp32")
+    sdkconfig_src = join(platform.get_package_dir("framework-arduinoespressif32"),"tools","esp32-arduino-libs",mcu,"sdkconfig")
+
+    def get_flag(line):
+        if line.startswith("#") and "is not set" in line:
+            return line.split(" ")[1]
+        elif not line.startswith("#") and len(line.split("=")) > 1:
+            return line.split("=")[0]
+        else:
+            return None
+
+    try:
+        idf_config_flags = env.GetProjectOption("custom_sdkconfig").splitlines()
+        print(idf_config_flags)
+    except:
+        idf_config_flags = []
+        print("no user additions")
+    with open(sdkconfig_src) as src:
+        sdkconfig_dst = join(env.subst("$PROJECT_DIR"),"sdkconfig.defaults")
+        dst = open(sdkconfig_dst,"w")
+        while line := src.readline():
+            flag = get_flag(line)
+            # print(flag)
+            if flag is None:
+                dst.write(line)
+            else:
+                no_match = True
+                for item in idf_config_flags:
+                    if flag in item:
+                        dst.write(item+"\n")
+                        no_match = False
+                        print("Replace:",line," with: ",item)
+                if no_match:
+                    dst.write(line)
+        dst.close()
+
+    print("Use sdkconfig from Arduino libs as default")
+    print(sdkconfig_src,sdkconfig_dst)
+    # shutil.copy(sdkconfig_src,sdkconfig_dst) # TODO: maybe no rude overwrite
+    # assert(0)
 
 # Pass flashmode at build time to macro
 memory_type = env.BoardConfig().get("build.arduino.memory_type", "").upper()
@@ -20,3 +72,6 @@ if "OPI_" in memory_type:
 tasmota_flash_mode = "-DCONFIG_TASMOTA_FLASHMODE_" + flash_mode
 env.Append(CXXFLAGS=[tasmota_flash_mode])
 print(tasmota_flash_mode)
+
+if "espidf" in env["PIOFRAMEWORK"]:
+    HandleArduinoIDFbuild(env)
