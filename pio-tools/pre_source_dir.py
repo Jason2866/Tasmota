beda09852a61ed68e9691659fa2461c3bc520cbb
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-09-30 20:55:09 +0200

    Update pre_source_dir.py

--- pio-tools/pre_source_dir.py
+++ pio-tools/pre_source_dir.py
@@ -39,83 +39,9 @@ env.Append(CXXFLAGS=[tasmota_flash_mode])
 print(tasmota_flash_mode)
 #########################################################
 
-def HandleArduinoIDFbuild(env, idf_config_flags):
-    print("Build customized Arduino libs!")
-    if mcu in ("esp32", "esp32s2", "esp32s3"):
-        env["BUILD_FLAGS"].append("-mtext-section-literals") # TODO
-    #print("Platform dir", os.path.join(env.subst("$PROJECT_CORE_DIR"), "platforms"))
-
-    #arduino_libs_mcu = join(FRAMEWORK_DIR,"tools","esp32-arduino-libs",mcu)
-    #lib_backup_folder = "lib_backup"
-    #if lib_backup_folder not in os.listdir(arduino_libs_mcu):
-        #destination = shutil.copytree(join(arduino_libs_mcu,"lib"), join(arduino_libs_mcu,lib_backup_folder), copy_function = shutil.copy)
-
-    sdkconfig_src = join(FRAMEWORK_DIR,"tools","esp32-arduino-libs",mcu,"sdkconfig")
-
-    def get_flag(line):
-        if line.startswith("#") and "is not set" in line:
-            return line.split(" ")[1]
-        elif not line.startswith("#") and len(line.split("=")) > 1:
-            return line.split("=")[0]
-        else:
-            return None
-
-    with open(sdkconfig_src) as src:
-        sdkconfig_dst = join(env.subst("$PROJECT_DIR"),"sdkconfig.defaults")
-        dst = open(sdkconfig_dst,"w")
-        dst.write("# TASMOTA\n")
-        while line := src.readline():
-            flag = get_flag(line)
-            # print(flag)
-            if flag is None:
-                dst.write(line)
-            else:
-                no_match = True
-                for item in idf_config_flags:
-                    if flag in item:
-                        dst.write(item+"\n")
-                        no_match = False
-                        print("Replace:",line," with: ",item)
-                if no_match:
-                    dst.write(line)
-        dst.close()
-
-    # print("Use sdkconfig from Arduino libs as default")
-    # print(sdkconfig_src,sdkconfig_dst)
-    # shutil.copy(sdkconfig_src,sdkconfig_dst) # TODO: maybe no rude overwrite
-    # assert(0)
-
-    EXTRA_IMG_DIR = join(env.subst("$PROJECT_DIR"), "variants", "tasmota")
-    env.Append(
-        FLASH_EXTRA_IMAGES=[
-            (offset, join(EXTRA_IMG_DIR, img)) for offset, img in board.get("upload.arduino.flash_extra_images", [])
-        ]
-    )
-
-def esp32_copy_new_arduino_libs(target, source, env):
-    print("Will copy new Arduino libs to .platformio")
-    lib_src = join(env["PROJECT_BUILD_DIR"],env["PIOENV"],"esp-idf")
-    lib_dst = join(FRAMEWORK_DIR,"tools","esp32-arduino-libs",mcu,"lib")
-    src = [join(lib_src,x) for x in os.listdir(lib_src)]
-    src = [folder for folder in src if not os.path.isfile(folder)] # folders only
-    for folder in src:
-        # print(folder)
-        files = [join(folder,x) for x in os.listdir(folder)]
-        for file in files:
-            if file.strip().endswith(".a"):
-                # print(file.split("/")[-1])
-                shutil.copyfile(file,join(lib_dst,file.split("/")[-1]))
-    if not bool(os.path.isfile(join(FRAMEWORK_DIR,"tools","esp32-arduino-libs",mcu,"sdkconfig.orig"))):
-        shutil.move(join(FRAMEWORK_DIR,"tools","esp32-arduino-libs",mcu,"sdkconfig"),join(FRAMEWORK_DIR,"tools","esp32-arduino-libs",mcu,"sdkconfig.orig"))
-    shutil.copyfile(join(env.subst("$PROJECT_DIR"),"sdkconfig."+env["PIOENV"]),join(FRAMEWORK_DIR,"tools","esp32-arduino-libs",mcu,"sdkconfig"))
-    exit() # TODO Post action in pre script!
-
-
 try:
     if idf_config_flags := env.GetProjectOption("custom_sdkconfig").splitlines():
         env["PIOFRAMEWORK"].append("espidf")
-        HandleArduinoIDFbuild(env, idf_config_flags)
-        env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", esp32_copy_new_arduino_libs) # TODO Post action in pre script!
 except:
     pass
     # arduino_libs_mcu = join(FRAMEWORK_DIR,"tools","esp32-arduino-libs",mcu)
