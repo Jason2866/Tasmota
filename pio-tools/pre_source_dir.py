89889742d4916f7255df0324c5445eb48304b5ff
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-07 12:17:56 +0200

    Update pre_source_dir.py

--- pio-tools/pre_source_dir.py
+++ pio-tools/pre_source_dir.py
@@ -2,9 +2,6 @@ Import("env")
 
 import glob
 import os
-import os.path
-from os.path import join
-import shutil
 
 platform = env.PioPlatform()
 board = env.BoardConfig()
@@ -14,13 +11,15 @@ def FindInoNodes(env):
     return env.Glob(os.path.join(src_dir, "*.ino")) + env.Glob(
         os.path.join(src_dir, "tasmota_*", "*.ino")
     )
+
 env.AddMethod(FindInoNodes)
 
 # Pass flashmode at build time to macro
-memory_type = board.get("build.arduino.memory_type", "").upper()
-flash_mode = board.get("build.flash_mode", "dio").upper()
+memory_type = env.BoardConfig().get("build.arduino.memory_type", "").upper()
+flash_mode = env.BoardConfig().get("build.flash_mode", "dio").upper()
 if "OPI_" in memory_type:
     flash_mode = "OPI"
+
 tasmota_flash_mode = "-DCONFIG_TASMOTA_FLASHMODE_" + flash_mode
 env.Append(CXXFLAGS=[tasmota_flash_mode])
 print(tasmota_flash_mode)
