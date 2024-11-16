11aeda485a092454a441ef24698522765d3b8303
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-11-16 23:44:39 +0100

    Update strip-flags.py

--- pio-tools/strip-flags.py
+++ pio-tools/strip-flags.py
@@ -3,17 +3,6 @@ Import('env')
 link_flags = env['LINKFLAGS']
 build_flags = " ".join(env['BUILD_FLAGS'])
 
-try:
-  link_flags.pop(link_flags.index("-u _printf_float"))
-  print("*** link flags:", link_flags)
-except:
-  pass
-try:
-  link_flags.pop(link_flags.index("-u _scanf_float"))
-  print("*** link flags:", link_flags)
-except:
-  pass
-
 if "FIRMWARE_SAFEBOOT" in build_flags:
   # Crash Recorder is not included in safeboot firmware -> remove Linker wrap
   try:
