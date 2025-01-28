9d4af3209812a328da92d763e531c5071b2b7054
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-01-28 17:36:43 +0100

    only >= IDF 5.3

--- tasmota/tasmota_xdrv_driver/xdrv_91_esp32_twai.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_91_esp32_twai.ino
@@ -7,6 +7,7 @@
 */
 
 #ifdef ESP32
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 3, 0)
 #ifdef USE_ESP32_TWAI
 #if SOC_TWAI_SUPPORTED
 /*********************************************************************************************\
@@ -421,4 +422,5 @@ bool Xdrv91(uint32_t function) {
 
 #endif  // SOC_TWAI_SUPPORTED
 #endif  // USE_ESP32_TWAI
+#endif  // ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 3, 0)
 #endif  // ESP32
