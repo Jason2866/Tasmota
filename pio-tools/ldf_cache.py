f6042f91b34c12b49246407c01f491ea809a52a0
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-07-16 22:10:56 +0200

    delete line and set to column 1

--- pio-tools/ldf_cache.py
+++ pio-tools/ldf_cache.py
@@ -1317,7 +1317,7 @@ if should_trigger_verbose_build() and not github_actions and not flag_custom_sdk
             sys.stdout.write("\033[2K")  # Delete complete line
             logfile.write(line)
             logfile.flush()
-        sys.stdout.write("\x1b[2K")
+        sys.stdout.write("\033[2K\r")
         sys.stdout.write("\033[F")
         logfile.flush()
         print("\n✅ Build process completed, waiting for process to finish...")
