bc552b40752681b4f275c6af03634eab4cba50aa
Jason2866 <obermeier.johann@googlemail.com>
2025-08-20 21:30:25 +0200

    better exclude macro for c5 in TWAI

--- tasmota/tasmota_xdrv_driver/xdrv_91_esp32_twai.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_91_esp32_twai.ino
@@ -7,7 +7,7 @@
 */
 
 #ifdef ESP32
-#ifndef ESP32C5 // todo: remove when ESP32C5 support is added
+#if !CONFIG_IDF_TARGET_ESP32C5 // todo: remove when ESP32C5 TWAI support is added
 #ifdef USE_ESP32_TWAI
 #if SOC_TWAI_SUPPORTED
 /*********************************************************************************************\
@@ -443,5 +443,5 @@ bool Xdrv91(uint32_t function) {
 
 #endif  // SOC_TWAI_SUPPORTED
 #endif  // USE_ESP32_TWAI
-#endif  // ESP32C5
+#endif  // !CONFIG_IDF_TARGET_ESP32C5
 #endif  // ESP32
